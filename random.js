03d8daff54946bb6e667408208ec0b800be8ce6a
Belial-FrontEnd-Dev <102366429+Belial-FrontEnd-Dev@users.noreply.github.com>
2022-10-25 08:49:37 +0200

    Add files via upload

---
+++ random.js
@@ -0,0 +1,45 @@
+const hamburgerMenu = document.querySelector(".hamburger")
+const menu = document.querySelector(".menu")
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active")
+  menu.classList.toggle("active")
+})
+
+const link = document.querySelectorAll(".nav-link")
+
+for (let i = 0; i < link.length; i++) {
+  link[i].addEventListener("click", () => {
+    hamburgerMenu.classList.toggle("active")
+    menu.classList.remove("active")
+  })
+}
+
+const btn = document.querySelector(".contactBtn")
+
+btn.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active")
+  menu.classList.remove("active")
+})
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+})
